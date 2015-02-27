57781bfc23161e115e50f2d78919792d66883b44
sagararathi <bravo.sagar@gmail.com>
2015-02-27 14:50:01 -0500

    add test for edit and update

--- spec/controllers/riddles_controller_spec.rb
+++ spec/controllers/riddles_controller_spec.rb
@@ -1,12 +1,7 @@
 require 'rails_helper'
 
-# before :each do
-#   stub_current_user
-
-# end
-
 describe RiddlesController do
-  let(:riddle) { FactoryGirl.create :riddle }
+   let(:riddle) { FactoryGirl.create :riddle }
 
   describe "#index" do
     it "assigns the riddles to Riddles.all" do
@@ -23,7 +18,7 @@ describe RiddlesController do
   end
 
   describe "#create" do
-    it "creates the todo if valid params" do
+    it "creates the riddle if valid params" do
 
       post :create, riddle: {
         title: 'Riddle 1',
@@ -33,7 +28,29 @@ describe RiddlesController do
       }
 
       expect(response).to redirect_to 'show'
-
     end
+  end
+
+  describe "#edit" do
+    it "assigns the riddle to the riddle" do
+      get :edit, id: riddle.id
+      expect(assigns(:riddle)).to eq(riddle)
+    end
+  end
+
+  describe "#update" do
+    it "updates the riddle if valid params" do
+      put :update, id: riddle.id,
+        riddle: {
+        title: 'Riddle 2',
+        body: 'Riddle"s to Riddle',
+        answer: 'Hard to answer',
+        user_id: 1
+      }
+
+      expect(response).to be_redirect
+    end
+
+
   end
 end
