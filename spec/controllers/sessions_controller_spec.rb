2c53e57e1ab619a7c010900132368f301c0c95a6
ivanyukio <ivan.yk14@gmail.com>
2015-02-27 10:42:43 -0500

    add session controller file and spec file

---
+++ spec/controllers/sessions_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe SessionsController do
+  context "new" do
+    it "is successful" do
+      get :new
+      expect(response).to be_success
+    end
+  end
+
+  context "create" do
+    let(:user){User.create(username: :test, email: "test@email.com", password: "123")}
+      it "" do
+
+      end
+  end
+end
