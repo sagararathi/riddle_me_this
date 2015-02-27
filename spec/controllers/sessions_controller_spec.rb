0ba1e7da2e93d0b5b2dfa17b26ec0cf88c25ba73
ivanyukio <ivan.yk14@gmail.com>
2015-02-27 11:52:30 -0500

    add session sontroller tests

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe SessionsController do
+describe "SessionsController" do
   context "new" do
     it "is successful" do
       get :new
@@ -9,9 +9,27 @@ describe SessionsController do
   end
 
   context "create" do
-    let(:user){User.create(username: :test, email: "test@email.com", password: "123")}
-      it "" do
+    let(:user){User.create(username: "test", email: "test@test.com", password: "123")}
+    it "redirects to root path if valid user" do
+      post :create, :username => user.username, email: user.email, :password => user.password
+      expect(response).to redirect_to root_path
+    end
+    it "redirects to sign in path with bad email" do
+      post :create, :username => user.username, email: "1", :password => user.password
+      expect(response).to redirect_to signup_new_path
+    end
+  end
 
-      end
+  context "login" do
+    let(:user){User.create(username: "test", email: "test@test.com", password: "123")}
+    it "redirects to root path ig login is valid" do
+      user
+      post :login, :username => user.username, email: user.email, :password => user.password
+      expect(response).to redirect_to root_path
+    end
+    it "redirects to login path if login is invalid" do
+      post :login, :username => "hey", email: "not an email"
+      expect(response).to redirect_to login_path
+    end
   end
 end
