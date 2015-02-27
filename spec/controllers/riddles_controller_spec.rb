e4c285456d49a3958ea0a4638ecb52bf837787ff
sagararathi <bravo.sagar@gmail.com>
2015-02-27 10:33:20 -0500

    Create test for new Riddle

--- spec/controllers/riddles_controller_spec.rb
+++ spec/controllers/riddles_controller_spec.rb
@@ -7,5 +7,15 @@ describe RiddlesController do
     it "assigns the riddles to Riddles.all" do
       get :index
       expect(response).to be_success
+    end
   end
+
+  describe "#new" do
+    it "assigns the riddle to Riddle.new " do
+      get :new
+      expect(assigns(:riddle)).to be_a_new(Riddle)
+    end
+  end
+
+
 end
