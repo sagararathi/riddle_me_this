ce5192afff61da98f18b7839430eb8aedd51716e
ivanyukio <ivan.yk14@gmail.com>
2015-02-28 16:06:14 -0500

    fix show/hide answer button

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,7 +1,12 @@
 $(document).ready(function(){
   $('.show_ans').on("submit", function(event){
     event.preventDefault()
-    $('#riddle_ans').show();
-    console.log("hey")
+    $('#riddle_ans').toggle();
+    if($('.ans_btn').attr('value') === "Show Answer"){
+      $('.ans_btn').attr('value', "Hide Answer")
+    } else {
+      $('.ans_btn').attr('value', "Show Answer")
+    }
+
   })
 })
