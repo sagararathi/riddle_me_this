c09b98cdb10041fe67630bcc85840da7e8db0e78
Kenn Breece <kjb085@gmail.com>
2015-02-28 13:32:29 -0500

    Temporarily commented out Sagar's riddle factory to work on test comments

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,33 +1,29 @@
 FactoryGirl.define do
-<<<<<<< HEAD
 	factory :comment do
 		body { Faker::Lorem.paragraph }
 		user_id { 1 }
 		riddle_id { 1 }
 	end
-end
 
-FactoryGirl.define do
 	factory :user do
 		username { Faker::Internet.user_name }
 		email { Faker::Internet.free_email }
 		password { "1234" }
 	end
-end
 
-FactoryGirl.define do
 	factory :riddle do
 		title { Faker::Lorem.sentence }
 		body { Faker::Lorem.sentence }
 		answer { Faker::Lorem.sentence }
 		user_id { 1 }
 	end
-=======
-  factory :riddle do
-     sequence(:title) { |n| "Title #{n}" }
-     body "Body Here"
-     answer "Answer here"
-    user_id '1'
-  end
->>>>>>> 3f2510e7bff7ca80c07e98f08d9e8d416d921619
+
+  # Factory that Sagar created, currently commented out for comment spec tests 
+  # factory :riddle do
+  #    sequence(:title) { |n| "Title #{n}" }
+  #    body "Body Here"
+  #    answer "Answer here"
+  #   user_id '1'
+  # end
+
 end
