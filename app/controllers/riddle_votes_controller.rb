325e541d13629e341cef5ccdce7a560e95022ca7
sagararathi <bravo.sagar@gmail.com>
2015-02-28 13:27:40 -0500

    add method to riddle_votes controllere for new, create and private method for riddle_votes_params

--- app/controllers/riddle_votes_controller.rb
+++ app/controllers/riddle_votes_controller.rb
@@ -1,11 +1,18 @@
 class RiddleVotesController < ApplicationController
 
 def new
-
+  @riddle_vote = RiddleVote.new
 end
 
 def create
+@riddle_vote = RiddleVote.where(riddle_votes_params).first_or_create
+end
 
+private
+
+def riddle_votes_params
+  params.require(:riddle).permit(:user_id, :riddle_id)
 end
 
 end
+
