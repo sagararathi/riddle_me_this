aea5d4ffec67624e86401009e452a9cddd0473dc
rosapergams <rosa@pergams.com>
2015-02-28 17:07:19 -0500

    Seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+NUM_USERS = 10
+NUM_RIDDLES = 77
+NUM_RIDDLE_VOTES = NUM_RIDDLES * 2
+NUM_COMMENTS = NUM_RIDDLES * 5
+NUM_COMMENT_VOTES = NUM_COMMENTS * 2
+
+(NUM_USERS).times do
+  User.create(
+    username: Faker::Internet.user_name,
+    email: Faker::Internet.email,
+    password: Faker::Internet.password
+    )
+end
+
+(NUM_COMMENTS).times do
+  Comment.create(
+    body: Faker::Lorem.paragraph,
+    riddle_id: rand(1..NUM_RIDDLES),
+    user_id: rand(1..NUM_USERS)
+    )
+end
+
+(NUM_RIDDLE_VOTES).times do
+  RiddleVote.create(
+    riddle_id: rand(1..NUM_RIDDLES),
+    user_id: rand(1..NUM_USERS)
+    )
+end
+
+(NUM_COMMENT_VOTES).times do
+  CommentVote.create(
+    comment_id: rand(1..NUM_COMMENTS),
+    user_id: rand(1..NUM_USERS)
+    )
+end
