3c2bb2824f4f4fa3c55b2ca2ff39629e4b62de8c
sagararathi <bravo.sagar@gmail.com>
2015-03-01 10:19:16 -0500

    changes made in class method top5 as suggested by steven

--- app/models/riddle.rb
+++ app/models/riddle.rb
@@ -3,23 +3,32 @@ class Riddle < ActiveRecord::Base
   has_many :comments
   has_many :riddle_votes
 
-  def self.top5 answered_only = false
+  #  def self.top5 answered_only = false
+  #   result = []
+  #   relation = select("riddles.id, count(riddle_votes.id) AS vote_count").
+  #   joins(:riddle_votes).
+  #   group("riddles.id").
+  #   order("vote_count DESC").
+  #   limit(5)
+  #   if answered_only
+  #     relation = relation.where('riddles.answer is not null')
+  #   end
 
-    result = []
-    relation = select("riddles.id, count(riddle_votes.id) AS vote_count").
+  #   relation.each do |rid|
+  #     result << Riddle.find(rid.id)
+  #   end
+  #   result
+  # end
+
+ def self.top5 answered_only = false
+    rel = select("riddles.id, riddles.title, riddles.body, riddles.answer, riddles.user_id, riddles.created_at, riddles.update_at, count(riddle_votes.id) as vote_count").
     joins(:riddle_votes).
-    group("riddles.id").
-    order("vote_count DESC").
+    group(["riddles.id", "riddles.title", "riddles.body", "riddles.answer", "riddles.user_id", "riddles.created_at", "riddles.update_at"]).
+    order(" vote_count DESC").
     limit(5)
-    if answered_only
-      relation = relation.where('riddles.answer is not null')
-    end
-
-    relation.each do |rid|
-      result << Riddle.find(rid.id)
-    end
-    result
+    rel = rel.where(answered_only: true) if answered_onlys
   end
+
 
   def self.all_in_order answered_only = false
     result = []
