c117052c862f9a530e0cc56d2c40610f8b4fccc2
ivanyukio <ivan.yk14@gmail.com>
2015-03-01 19:03:53 -0500

    add ajax for comments upvote and riddles upvote

--- app/controllers/riddle_votes_controller.rb
+++ app/controllers/riddle_votes_controller.rb
@@ -1,17 +1,19 @@
 class RiddleVotesController < ApplicationController
 
+  def create
+    @riddle_vote = RiddleVote.where(riddle_id: params[:riddle_id], user_id: current_user.id).first_or_create
+    number = Riddle.find_by(id: params[:riddle_id]).riddle_votes.count
+    respond_to do |format|
+      format.html{redirect_to @riddle_vote.riddle}
+      format.json{render json: number}
+    end
+  end
 
+  private
 
-def create
-@riddle_vote = RiddleVote.where(riddle_id: params[:riddle_id], user_id: current_user.id).first_or_create
-redirect_to @riddle_vote.riddle
-end
-
-private
-
-def riddle_votes_params
-  params.require(:riddle).permit(:riddle_id).merge(user_id: current_user.id)
-end
+  def riddle_votes_params
+    params.require(:riddle).permit(:riddle_id).merge(user_id: current_user.id)
+  end
 
 end
 
